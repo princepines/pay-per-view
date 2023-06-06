74dabf0f03e8914bba04fc5bead016be9245079e
Frizth Lyco Tatierra <lycol50@gmail.com>
2023-06-06 15:50:47 +0000

    test hls.js so many times

--- event.php
+++ event.php
@@ -22,30 +22,31 @@ if (!isset($_SESSION['loggedin'])) {
     <link rel="icon" type="image/x-icon" href="white.png">
     <script src="https://hlsjs.video-dev.org/dist/hls.js"></script>
 </head>
+
 <body>
     <?php require 'nav.php';?>
     <div class="container">
         <div class="row">
             <div class="col">
-                <video id="video"></video>
+                <video id="video" controls></video>
                 <script>
                 var video = document.getElementById('video');
                 if (Hls.isSupported()) {
-                    var hls = new Hls();
-                    hls.loadSource(
-                        'https://streaming.cnnphilippines.com/live/myStream/playlist.m3u8'
-                        );
+                    var hls = new Hls({
+                        debug: true,
+                    });
+                    hls.loadSource('https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8');
                     hls.attachMedia(video);
-                    hls.on(Hls.Events.MANIFEST_PARSED, function() {
+                    hls.on(Hls.Events.MEDIA_ATTACHED, function() {
+                        video.muted = true;
                         video.play();
                     });
                 }
                 // hls.js is not supported on platforms that do not have Media Source Extensions (MSE) enabled.
-                // When the browser has built-in HLS support (check using `canPlayType`), we can provide an HLS manifest (i.e. .m3u8 URL) directly to the video element throught the `src` property.
+                // When the browser has built-in HLS support (check using `canPlayType`), we can provide an HLS manifest (i.e. .m3u8 URL) directly to the video element through the `src` property.
                 // This is using the built-in support of the plain video element, without using hls.js.
                 else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-                    video.src =
-                        'https://streaming.cnnphilippines.com/live/myStream/playlist.m3u8';
+                    video.src = 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8';
                     video.addEventListener('canplay', function() {
                         video.play();
                     });
