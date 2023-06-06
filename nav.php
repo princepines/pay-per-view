ab95e74bc346e3df3752b2991c05ddf14871ab42
Frizth Lyco Tatierra <lycol50@gmail.com>
2023-06-06 15:25:05 +0000

    navbar

--- nav.php
+++ nav.php
@@ -4,10 +4,10 @@
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
-      <a class="nav-item nav-link active" href="/">Home</a>
-      <a class="nav-item nav-link" href="https://tsibml.mooo.com" target="_blank">Stats</a>  
-      <a class="nav-item nav-link" href="about.php">About</a> 
-    </div>
+        <div class="navbar-nav">
+            <a class="nav-item nav-link" href="/">Home</a>
+            <a class="nav-item nav-link" href="https://tsibml.mooo.com" target="_blank">Stats</a>  
+            <a class="nav-item nav-link" href="about.php">About</a> 
+     </div>
   </div>
 </nav>
