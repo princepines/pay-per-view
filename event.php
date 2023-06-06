0f663c84c93d179bd013c4ce8cf45a63dc3d2bcc
Frizth Lyco Tatierra <lycol50@gmail.com>
2023-06-06 18:42:52 +0000

    move sensitive details to env

--- event.php
+++ event.php
@@ -1,4 +1,11 @@
 <?php
+require 'vendor/autoload.php';
+
+use Dotenv\Dotenv;
+// initialize dotenv and variables
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->safeLoad();
+
 // We need to use sessions, so you should always start sessions using the below code.
 session_start();
 if (!isset($_SESSION['loggedin'])) {
@@ -34,10 +41,10 @@ if (!isset($_SESSION['loggedin'])) {
                 var hls = new Hls({
                     debug: true,
                 });
-                hls.loadSource('https://streaming.cnnphilippines.com/live/myStream/playlist.m3u8');
+                hls.loadSource('<?php echo $_ENV['STREAMURL'];?>');
                 hls.attachMedia(video);
                 hls.on(Hls.Events.MEDIA_ATTACHED, function() {
-                    video.muted = true;
+                    video.muted = false;
                     video.play();
                 });
             }
@@ -45,7 +52,7 @@ if (!isset($_SESSION['loggedin'])) {
             // When the browser has built-in HLS support (check using `canPlayType`), we can provide an HLS manifest (i.e. .m3u8 URL) directly to the video element through the `src` property.
             // This is using the built-in support of the plain video element, without using hls.js.
             else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-                video.src = 'https://streaming.cnnphilippines.com/live/myStream/playlist.m3u8';
+                video.src = '<?php echo $_ENV['STREAMURL'];?>';
                 video.addEventListener('canplay', function() {
                     video.play();
                 });
@@ -53,9 +60,8 @@ if (!isset($_SESSION['loggedin'])) {
             </script>
         </div><br>
         <div class="row">
-        <script id="cid0020000347066667544" data-cfasync="false" async src="//st.chatango.com/js/gz/emb.js" style="width: 100%;height: 300px;">{"handle":"ibml","arch":"js","styles":{"a":"121212","b":100,"c":"FFFFFF","d":"FFFFFF","k":"121212","l":"121212","m":"121212","n":"FFFFFF","p":"13.41","q":"121212","r":100,"t":0,"usricon":0,"surl":0,"allowpm":0,"cnrs":"0.37"}}</script>
+        <script id="<?php echo $_ENV['EMBEDID_CHATANGO'];?>" data-cfasync="false" async src="//st.chatango.com/js/gz/emb.js" style="width: 100%;height: 300px;">{"handle":"ibml","arch":"js","styles":{"a":"121212","b":100,"c":"FFFFFF","d":"FFFFFF","k":"121212","l":"121212","m":"121212","n":"FFFFFF","p":"13.41","q":"121212","r":100,"t":0,"usricon":0,"surl":0,"allowpm":0,"cnrs":"0.37"}}</script>
         </div>
     </div>
 </body>
-
 </html>
