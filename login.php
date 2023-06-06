9c2b6b2da3e8b3711e39769f2d48498d0bc7e0ba
Frizth Lyco Tatierra <lycol50@gmail.com>
2023-06-06 14:43:53 +0000

    last scaling

--- login.php
+++ login.php
@@ -98,7 +98,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <body>
 	<div class="container">
 		<div class="row">
-		<img src="poster.png" alt="K4th Production" style="height:300px; object-fit: cover; object-position: 100% 20%">
+		<img src="poster.png" alt="K4th Production" style="height:300px; object-fit: cover; object-position: 100% 25%">
 			<div class="col">
 				<h1>Login to the event</h1>
 				<p>Please fill in your code to login.</p>
