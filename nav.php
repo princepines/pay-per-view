a93786088af5650a4fa199f70c22ef9dab7afbfd
Frizth Lyco Tatierra <lycol50@gmail.com>
2023-06-06 15:22:02 +0000

    navbar

--- nav.php
+++ nav.php
@@ -6,13 +6,8 @@
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div class="navbar-nav">
       <a class="nav-item nav-link active" href="/">Home</a>
-      <a class="nav-item nav-link" href="/SIS/forms/student/subjects.php">Subjects and Grades</a>
-      <a class="nav-item nav-link" href="/SIS/forms/student/payments.php">Payments</a>
-      <a class="nav-item nav-link" href="/SIS/about.php">About</a>
-      <a class="nav-item nav-link" href="/SIS/logout.php">Logout</a>      
+      <a class="nav-item nav-link" href="https://tsibml.mooo.com" target="_blank">Stats</a>  
+      <a class="nav-item nav-link" href="about.php">About</a> 
     </div>
   </div>
-  <span class="navbar-text" style="font-family: Kanit, sans-serif;">
-      [Student]
-  </span>
 </nav>
