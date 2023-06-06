c73df221ed2125aa84af8e3b28c86753861be9ea
Frizth Lyco Tatierra <lycol50@gmail.com>
2023-06-06 12:51:09 +0000

    login redirect too much

--- login.php
+++ login.php
@@ -7,11 +7,6 @@ if (isset($_SESSION['loggedin'])) {
 	exit;
 }
 
-// If the user is not logged in redirect to the login page...
-if (!isset($_SESSION['loggedin'])) {
-	header('Location: login.php');
-	exit;
-}
 
 require_once "config.php";
 
