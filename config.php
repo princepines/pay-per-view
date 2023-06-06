547908040f34161f1624a5afa3c4ae2d59346490
Frizth Lyco Tatierra <lycol50@gmail.com>
2023-06-06 12:57:33 +0000

    composer issues

--- config.php
+++ config.php
@@ -1,6 +1,9 @@
 <?php
+require 'vendor/autoload.php';
+
+use Dotenv\Dotenv;
 // initialize dotenv and variables
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv = Dotenv/Dotenv::createImmutable(__DIR__);
 $dotenv->safeLoad();
 
 /* Database credentials. Assuming you are running MySQL
