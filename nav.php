b1219529925dbb3a74afe660917e33545c13d6a5
Frizth Lyco Tatierra <lycol50@gmail.com>
2023-06-06 15:15:52 +0000

    event

---
+++ nav.php
@@ -0,0 +1,18 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#"><img src="techsparks.png" alt="TechSparks Logo"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link active" href="/">Home</a>
+      <a class="nav-item nav-link" href="/SIS/forms/student/subjects.php">Subjects and Grades</a>
+      <a class="nav-item nav-link" href="/SIS/forms/student/payments.php">Payments</a>
+      <a class="nav-item nav-link" href="/SIS/about.php">About</a>
+      <a class="nav-item nav-link" href="/SIS/logout.php">Logout</a>      
+    </div>
+  </div>
+  <span class="navbar-text" style="font-family: Kanit, sans-serif;">
+      [Student]
+  </span>
+</nav>
