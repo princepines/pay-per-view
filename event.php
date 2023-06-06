cf56021b9770f874c352be2312ca175d6e9a168a
Frizth Lyco Tatierra <lycol50@gmail.com>
2023-06-06 15:41:22 +0000

    formatting

--- event.php
+++ event.php
@@ -25,32 +25,33 @@ if (!isset($_SESSION['loggedin'])) {
 
 <body>
     <?php require 'nav.php';?>
-    <script>
-    var video = document.getElementById('video');
-    if (Hls.isSupported()) {
-        var hls = new Hls();
-        hls.loadSource(
-            'https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8');
-        hls.attachMedia(video);
-        hls.on(Hls.Events.MANIFEST_PARSED, function() {
-            video.play();
-        });
-    }
-    // hls.js is not supported on platforms that do not have Media Source Extensions (MSE) enabled.
-    // When the browser has built-in HLS support (check using `canPlayType`), we can provide an HLS manifest (i.e. .m3u8 URL) directly to the video element throught the `src` property.
-    // This is using the built-in support of the plain video element, without using hls.js.
-    else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-        video.src =
-            'https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8';
-        video.addEventListener('canplay', function() {
-            video.play();
-        });
-    }
-    </script>
     <div class="container">
         <div class="row">
             <div class="col">
                 <video id="video"></video>
+                <script>
+                var video = document.getElementById('video');
+                if (Hls.isSupported()) {
+                    var hls = new Hls();
+                    hls.loadSource(
+                        'https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8'
+                        );
+                    hls.attachMedia(video);
+                    hls.on(Hls.Events.MANIFEST_PARSED, function() {
+                        video.play();
+                    });
+                }
+                // hls.js is not supported on platforms that do not have Media Source Extensions (MSE) enabled.
+                // When the browser has built-in HLS support (check using `canPlayType`), we can provide an HLS manifest (i.e. .m3u8 URL) directly to the video element throught the `src` property.
+                // This is using the built-in support of the plain video element, without using hls.js.
+                else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+                    video.src =
+                        'https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8';
+                    video.addEventListener('canplay', function() {
+                        video.play();
+                    });
+                }
+                </script>
             </div>
         </div>
     </div>
