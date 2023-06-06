f9d5b8ea0e1cd1e67e51a903e33db79428382856
Frizth Lyco Tatierra <lycol50@gmail.com>
2023-06-06 15:02:16 +0000

    div

--- login.php
+++ login.php
@@ -98,7 +98,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <body>
 	<div class="container">
 		<div class="row">
-		<img src="poster.png" alt="K4th Production">
+			<div>
+				<img src="poster.png" alt="K4th Production">
+			</div>
 			<div class="col">
 				<h1>Login to the event</h1>
 				<p>Please fill in your code to login.</p>
