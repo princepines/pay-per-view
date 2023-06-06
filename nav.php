21919b16e4fa44357afad56887489dfa8770f4a2
Frizth Lyco Tatierra <lycol50@gmail.com>
2023-06-06 15:19:31 +0000

    logo

--- nav.php
+++ nav.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#"><img src="techsparks.png" alt="TechSparks Logo" width="50px"></a>
+  <a class="navbar-brand" href="#"><img src="techsparks.png" alt="TechSparks Logo" width="100px"></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
