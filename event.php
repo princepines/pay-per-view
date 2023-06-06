e97763410b0fb2470555a4477cddd43e3869e99d
Frizth Lyco Tatierra <lycol50@gmail.com>
2023-06-06 15:55:35 +0000

    height last

--- event.php
+++ event.php
@@ -28,7 +28,7 @@ if (!isset($_SESSION['loggedin'])) {
     <div class="container">
         <div class="row">
             <div class="col">
-                <video id="video" controls height="650"></video>
+                <video id="video" controls height="600"></video>
                 <script>
                 var video = document.getElementById('video');
                 if (Hls.isSupported()) {
