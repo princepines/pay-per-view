c5fa7dbc9221f2e729e1e2318a3c499a44a5ed77
Frizth Lyco Tatierra <lycol50@gmail.com>
2023-06-06 18:16:46 +0000

    seizing

--- event.php
+++ event.php
@@ -53,7 +53,7 @@ if (!isset($_SESSION['loggedin'])) {
             </script>
         </div><br>
         <div class="row">
-        <script id="cid0020000347066520029" data-cfasync="false" async src="//st.chatango.com/js/gz/emb.js" style="width: 100%;height: 300px;">{"handle":"ibml","arch":"js","styles":{"a":"121212","b":100,"c":"FFFFFF","d":"FFFFFF","k":"121212","l":"121212","m":"121212","n":"FFFFFF","p":"10","q":"121212","r":100,"t":0,"usricon":0.86,"allowpm":0,"cnrs":"0.35"}}</script>
+        <script id="cid0020000347066667544" data-cfasync="false" async src="//st.chatango.com/js/gz/emb.js" style="width: 100%;height: 300px;">{"handle":"ibml","arch":"js","styles":{"a":"121212","b":100,"c":"FFFFFF","d":"FFFFFF","k":"121212","l":"121212","m":"121212","n":"FFFFFF","p":"13.41","q":"121212","r":100,"t":0,"usricon":0,"surl":0,"allowpm":0,"cnrs":"0.37"}}</script>
         </div>
     </div>
 </body>
