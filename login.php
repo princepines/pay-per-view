e73d4bf61e451f7d1ae2eff97cfa6cab1fa7635d
Frizth Lyco Tatierra <lycol50@gmail.com>
2023-06-07 07:01:48 +0000

    added messenger popup

--- login.php
+++ login.php
@@ -127,6 +127,36 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 </head>
 
 <body>
+<!-- Messenger Chat Plugin Code -->
+<div id="fb-root"></div>
+
+<!-- Your Chat Plugin code -->
+<div id="fb-customer-chat" class="fb-customerchat">
+</div>
+
+<script>
+  var chatbox = document.getElementById('fb-customer-chat');
+  chatbox.setAttribute("page_id", "114546254986031");
+  chatbox.setAttribute("attribution", "biz_inbox");
+</script>
+
+<!-- Your SDK code -->
+<script>
+  window.fbAsyncInit = function() {
+	FB.init({
+	  xfbml            : true,
+	  version          : 'v17.0'
+	});
+  };
+
+  (function(d, s, id) {
+	var js, fjs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id)) return;
+	js = d.createElement(s); js.id = id;
+	js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+	fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+</script>
     <div class="container">
         <div class="row">
             <img src="poster.png" alt="K4th Production">
